a5bdcaaf42a10edd553976d0afae5a8e1813a9fb
Johnny Ruiz <johnny@ruizops.com>
2024-08-07 17:58:58 -0600

    added textinput bubble component for device

--- device/device.go
+++ device/device.go
@@ -1,54 +1,62 @@
 package device
 
 import (
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 type Model struct {
-	name        string
-	description string
-	mac_address string
-	ip_address  string
+	textInput textinput.Model
+	err       error
 }
 
+type (
+	errMsg error
+)
+
 func InitialModel() tea.Model {
+	ti := textinput.New()
+	ti.Placeholder = "PC Name"
+	ti.Focus()
+	ti.CharLimit = 156
+	ti.Width = 20
+
 	return Model{
-		name:        "",
-		description: "",
-		mac_address: "",
-		ip_address:  "",
+		textInput: ti,
+		err:       nil,
 	}
 }
 
 func (m Model) Init() tea.Cmd {
-	// Just return `nil`, which means "no I/O right now".
-	return nil
+	return textinput.Blink
 }
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
 
 	switch msg := msg.(type) {
-
-	// Check if it was a key press
 	case tea.KeyMsg:
-
-		// Check which key was pressed
-		switch msg.String() {
-
-		// These keys should exit the program.
-		case "ctrl+c", "q":
+		switch msg.Type {
+		case tea.KeyEnter, tea.KeyCtrlC, tea.KeyEsc:
 			return m, tea.Quit
 		}
+
+	// We handle errors just like any other message
+	case errMsg:
+		m.err = msg
+		return m, nil
 	}
 
-	// Return the updated Model to the Bubble Tea runtime for processing.
-	// Note that we're not returning a command.
-	return m, nil
+	m.textInput, cmd = m.textInput.Update(msg)
+	return m, cmd
+
 }
 
 func (m Model) View() string {
 	// The header
 	s := "New Device\n\n"
+	s += m.textInput.View() + "\n"
+	s += "\nPress esc to cancel."
 	s += "\nPress q to quit.\n"
 
 	return s
--- go.mod
+++ go.mod
@@ -5,6 +5,17 @@ go 1.22.6
 require github.com/charmbracelet/bubbletea v0.26.6
 
 require (
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/lipgloss v0.9.1 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+)
+
+require (
+	github.com/charmbracelet/bubbles v0.18.0
 	github.com/charmbracelet/x/ansi v0.1.2 // indirect
 	github.com/charmbracelet/x/input v0.1.0 // indirect
 	github.com/charmbracelet/x/term v0.1.1 // indirect
