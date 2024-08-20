33137b258830f1fabf0dc455ee75773c1e509456
Johnny Ruiz <johnny@ruizops.com>
2024-08-20 12:43:41 -0600

    moved keybindings to its own file

--- list/list.go
+++ list/list.go
@@ -14,61 +14,6 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-// keyMap defines a set of keybindings. To work for help it must satisfy
-// key.Map. It could also very easily be a map[string]key.Binding.
-type keyMap struct {
-	Up    key.Binding
-	Down  key.Binding
-	Enter key.Binding
-	New   key.Binding
-	Help  key.Binding
-	Quit  key.Binding
-}
-
-// ShortHelp returns keybindings to be shown in the mini help view. It's part
-// of the key.Map interface.
-func (k keyMap) ShortHelp() []key.Binding {
-	return []key.Binding{k.New, k.Help, k.Quit}
-}
-
-// FullHelp returns keybindings for the expanded help view. It's part of the
-// key.Map interface.
-func (k keyMap) FullHelp() [][]key.Binding {
-	return [][]key.Binding{
-		{k.Up, k.Down},   // first column
-		{k.Enter, k.New}, // second column
-		{k.Help, k.Quit}, // third column
-	}
-}
-
-// Keybindings for the Device component
-var keys = keyMap{
-	Up: key.NewBinding(
-		key.WithKeys("up", "k"),
-		key.WithHelp("↑/k", "move up"),
-	),
-	Down: key.NewBinding(
-		key.WithKeys("down", "j"),
-		key.WithHelp("↓/j", "move down"),
-	),
-	Enter: key.NewBinding(
-		key.WithKeys("enter"),
-		key.WithHelp("enter", "select"),
-	),
-	New: key.NewBinding(
-		key.WithKeys("n"),
-		key.WithHelp("n", "new device"),
-	),
-	Help: key.NewBinding(
-		key.WithKeys("?"),
-		key.WithHelp("?", "toggle help"),
-	),
-	Quit: key.NewBinding(
-		key.WithKeys("q", "esc", "ctrl+c"),
-		key.WithHelp("q", "quit"),
-	),
-}
-
 // Model for the Device component
 type Model struct {
 	devices []config.Device // list of devices to wake
