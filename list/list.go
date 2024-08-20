b2bbebaefa0c58449471d1d8739d17a89d961bf1
Johnny Ruiz <johnny@ruizops.com>
2024-08-20 13:51:01 -0600

    Implemented Wake-on-LAN (#8)
    * added rows to table
    
    * added wake on lan
    
    * submit message stating which device was sent a magic packet
    
    * added modal and returns to list after completed
    
    * auto close popup after 3 seconds
    
    * made some changes
    
    * moved keybindings to its own file
    
    * styled status bar and removed popup.go

--- list/list.go
+++ list/list.go
@@ -5,6 +5,7 @@ import (
 	"wakey/config"
 	"wakey/device"
 	"wakey/style"
+	"wakey/wol"
 
 	"github.com/charmbracelet/bubbles/help"
 	"github.com/charmbracelet/bubbles/key"
@@ -12,67 +13,13 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-// keyMap defines a set of keybindings. To work for help it must satisfy
-// key.Map. It could also very easily be a map[string]key.Binding.
-type keyMap struct {
-	Up    key.Binding
-	Down  key.Binding
-	Enter key.Binding
-	New   key.Binding
-	Help  key.Binding
-	Quit  key.Binding
-}
-
-// ShortHelp returns keybindings to be shown in the mini help view. It's part
-// of the key.Map interface.
-func (k keyMap) ShortHelp() []key.Binding {
-	return []key.Binding{k.New, k.Help, k.Quit}
-}
-
-// FullHelp returns keybindings for the expanded help view. It's part of the
-// key.Map interface.
-func (k keyMap) FullHelp() [][]key.Binding {
-	return [][]key.Binding{
-		{k.Up, k.Down},   // first column
-		{k.Enter, k.New}, // second column
-		{k.Help, k.Quit}, // third column
-	}
-}
-
-// Keybindings for the Device component
-var keys = keyMap{
-	Up: key.NewBinding(
-		key.WithKeys("up", "k"),
-		key.WithHelp("↑/k", "move up"),
-	),
-	Down: key.NewBinding(
-		key.WithKeys("down", "j"),
-		key.WithHelp("↓/j", "move down"),
-	),
-	Enter: key.NewBinding(
-		key.WithKeys("enter"),
-		key.WithHelp("enter", "toggle select"),
-	),
-	New: key.NewBinding(
-		key.WithKeys("n"),
-		key.WithHelp("n", "new device"),
-	),
-	Help: key.NewBinding(
-		key.WithKeys("?"),
-		key.WithHelp("?", "toggle help"),
-	),
-	Quit: key.NewBinding(
-		key.WithKeys("q", "esc", "ctrl+c"),
-		key.WithHelp("q", "quit"),
-	),
-}
-
 // Model for the Device component
 type Model struct {
 	devices []config.Device // list of devices to wake
 	keys    keyMap
 	help    help.Model
 	table   table.Model
+	status  string
 }
 
 // InitialModel function for the Device model
@@ -140,6 +87,16 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	// Update the table with the new rows
 	m.table.SetRows(rows)
 
+	// Define table rows
+	for i, device := range config.ReadConfig().Devices {
+		m.table.Rows()[i] = table.Row{
+			device.DeviceName,
+			device.Description,
+			device.MacAddress,
+			device.IPAddress,
+		}
+	}
+
 	switch msg := msg.(type) {
 	// Check if it was a key press
 	case tea.KeyMsg:
@@ -156,6 +113,21 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		// Toggle help
 		case key.Matches(msg, m.keys.Help):
 			m.help.ShowAll = !m.help.ShowAll
+
+		// Wake device
+		case key.Matches(msg, m.keys.Enter):
+			// Get the selected device
+			selected := m.table.SelectedRow()
+
+			// Get the device MAC address
+			macAddress := selected[2]
+
+			// Wake the device
+			wol.WakeDevice(macAddress)
+
+			// Write the status message
+			deviceName := selected[0]
+			m.status = "Waking up " + deviceName + " (" + macAddress + ")"
 		}
 	}
 
@@ -191,7 +163,10 @@ func (m Model) View() string {
 	s += m.table.View() + "\n"
 
 	// Show device count
-	s += style.DeviceCountStyle.Render("Number of devices: "+strconv.Itoa(len(m.table.Rows()))) + "\n\n" // srtconv.Itoa converts int to string
+	s += style.DeviceCountStyle.Render(" Number of devices: "+strconv.Itoa(len(m.table.Rows()))) + "\n" // srtconv.Itoa converts int to string
+
+	// Status message
+	s += style.StatusStyle.Render("Status: "+style.StatusMessageStyle.Render(m.status)) + "\n"
 
 	// Help text
 	s += m.help.View(m.keys)
