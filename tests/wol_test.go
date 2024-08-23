25c2315b712690ca09b91d855e102cebc507dcd5
Johnny Ruiz <johnny@ruizops.com>
2024-08-23 10:54:26 -0600

    Fixed Tests, Pipeline and README (#18)
    * updated test
    
    * added azure IP
    
    * updated IP to cloudflare
    
    * only testing 1 test in pipeline
    
    * ref Github token
    
    * changed to secrets
    
    * removed linux build from pipeline
    
    * edit build file names
    
    * SHA is only for MACOS
    
    * added SHA for Windows and Linux
    
    * updated version for setup-go
    
    * added homebrew tap
    
    * updated hombrew formula
    
    * added escape character for quotes in Formula
    
    * removed unecessary directory
    
    * added auth for pushing to repo
    
    * added push origin to main
    
    * added TOKEN
    
    * moved HOMEBREW TOKEN to correct step
    
    * removed Homebrew Token
    
    * added origin to main
    
    * added GITHUB token
    
    * set auth token
    
    * Modified auth
    
    * fixed username
    
    * changed release.yaml
    
    * changed calling tap token
    
    * update push commit
    
    * fixed tap file
    
    * updated README

--- tests/wol_test.go
+++ tests/wol_test.go
@@ -6,9 +6,11 @@ import (
 )
 
 func TestIsOnline(t *testing.T) {
+	ipAddress := "1.1.1.1"
+
 	// Note: This is a simple test and may need to be adjusted for your environment
-	online := wol.IsOnline("1.1.1.1")
+	online := wol.IsOnline(ipAddress)
 	if !online {
-		t.Errorf("Expected 1.1.1.1 to be online")
+		t.Errorf("Expected %s to be online", ipAddress)
 	}
 }
