cc011207249bbca94eb12f86e82f3fa0f8bfa7fe
Johnny Ruiz <johnny@ruizops.com>
2024-09-09 14:15:58 -0600

    added validation (#38)

--- internal/device/validate.go
+++ internal/device/validate.go
@@ -3,6 +3,7 @@ package device
 import (
 	"fmt"
 	"regexp"
+	"strings"
 )
 
 func (m *Model) deviceNameValidator(value string) error {
@@ -56,6 +57,22 @@ func (m *Model) ipAddressValidator(value string) error {
 	}
 
 	m.err[3] = nil
+	return nil
+}
+
+func (m *Model) groupValidator(value string) error {
+
+	// Split the value string into an array
+	groupNames := strings.Split(value, ", ")
+
+	// Check if each group exists
+	for _, groupName := range groupNames {
+		if _, ok := m.groupNameMap[groupName]; !ok {
+			return fmt.Errorf("'%s' group does not exist", groupName)
+		}
+	}
+
+	m.err[4] = nil
 	return nil
 }
 
