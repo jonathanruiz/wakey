39094d50cd3e5c38d9e9ea552db830513fa6d6f5
Johnny Ruiz <johnny@ruizops.com>
2026-03-30 23:43:42 -0600

    order device in groups based on when it was added

--- internal/config/config.go
+++ internal/config/config.go
@@ -156,7 +156,7 @@ func readGroups(conn *sql.DB) ([]Group, error) {
 		SELECT g.id, g.group_name, gd.device_id
 		FROM groups g
 		LEFT JOIN group_devices gd ON g.id = gd.group_id
-		ORDER BY g.rowid
+		ORDER BY g.rowid, gd.rowid
 	`)
 	if err != nil {
 		return nil, err
